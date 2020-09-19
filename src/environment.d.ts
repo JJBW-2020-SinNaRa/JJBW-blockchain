a6a5f5d9f027613c78db17994d4c79b4732072b6
Pinot. Kim <pinot.kim@kakao.com>
2020-09-19 23:06:03 +0900

    :wrench: Add env for KAS

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,5 +3,10 @@ declare module NodeJS {
   export interface ProcessEnv {
     PORT: string;
     NODE_ENV: "production" | "staging" | "development" | "local";
+    KAS_ID: string;
+    KAS_SECRET: string;
+    KAS_URL: string;
+    KAS_CHAIN: 1001 | 8217;
+    CONTRACT_ADDRESS: string;
   }
 }
